f95033d07f62063c54900b676f38d275e90ffd64
Luisa Ibele <80256905+LuisaIb@users.noreply.github.com>
2023-09-01 23:00:48 +0200

    styling slides

--- src/eda.py
+++ src/eda.py
@@ -6,6 +6,7 @@ import numpy as np
 import random
 import plotly.graph_objects as go
 import torch
+from plotly.subplots import make_subplots
 
 class EDA:
     def __init__(self) -> None:
@@ -51,58 +52,109 @@ class EDA:
         plt.show()
 
     # Function to display class distribution
-    def display_class_distribution(self, dataset):
-        # Count the number of samples in each class
-        class_counts = torch.bincount(dataset.targets, minlength=10)
-        
-        # Create a bar plot to visualize the class distribution
-        fig = go.Figure(data=[go.Bar(
-            x=self.class_labels,
-            y=class_counts,
-            marker_color='#ff6900'
-        )])
+    def plot_class_distribution(self, trainset, testset):
+        # Compute class counts for the training and test sets
+        train_class_counts = torch.bincount(trainset.targets, minlength=10)
+        test_class_counts = torch.bincount(testset.targets, minlength=10)
 
-        fig.update_layout(
-            xaxis_title='Class',
-            yaxis_title='Number of Samples',
-            title='Class Distribution',
-            xaxis=dict(tickangle=45),
-            showlegend=False,
-            margin=dict(t=80, b=80, l=80, r=80)
+        # Create bar chart traces for the training and test sets
+        trace1 = go.Bar(
+            x=trainset.classes,
+            y=train_class_counts,
+            marker_color='#ff6900',
+            name='Train Set'
         )
 
+        trace2 = go.Bar(
+            x=testset.classes,
+            y=test_class_counts,
+            marker_color='#ff6900',
+            name='Test Set'
+        )
+
+        fig = make_subplots(rows=2, cols=1, subplot_titles=('Train Set', 'Test Set'))
+
+        fig.add_trace(trace1, row=1, col=1)
+        fig.add_trace(trace2, row=2, col=1)
+
+        fig.update_layout(
+            title='Class Distributions for Train and Test Sets',
+            xaxis=dict(tickangle=45),
+            showlegend=False,
+            barmode='group',
+            margin=dict(t=80, b=80, l=30, r=20)
+        )
+
+        fig.update_xaxes(tickangle=45, row=2, col=1)
+        fig.update_layout(height=600)
+        fig.update_layout(titlefont=dict(size=16, color='#000000'))
+        fig.update_yaxes(tickfont_color='#000000', row=1, col=1)
+        fig.update_yaxes(tickfont_color='#000000', row=2, col=1)
+        fig.update_xaxes(tickfont_color='#000000', row=1, col=1)
+        fig.update_xaxes(tickfont_color='#000000', row=2, col=1)
+
+        # Show the subplot figure
         fig.show()
 
-
     # Function to display average black pixels
-    def display_average_black_pixels(self, dataset):
+    def display_average_black_pixels(self, trainset, testset):
         # Calculate the average number of black pixels per class
-        class_pixel_averages = []
+        class_pixel_averages_train = []
+        class_pixel_averages_test = []
 
         for class_label in range(10):
-            class_indices = torch.where(dataset.targets == class_label)[0]
-            class_images = dataset.data[class_indices]
+            class_indices = torch.where(trainset.targets == class_label)[0]
+            class_images = trainset.data[class_indices]
             class_black_pixels = class_images.eq(0).sum(dim=(1, 2))
-            class_pixel_averages.append(class_black_pixels.float().mean().item())
+            class_pixel_averages_train.append(class_black_pixels.float().mean().item())
+
+        for class_label in range(10):
+            class_indices = torch.where(testset.targets == class_label)[0]
+            class_images = testset.data[class_indices]
+            class_black_pixels = class_images.eq(0).sum(dim=(1, 2))
+            class_pixel_averages_test.append(class_black_pixels.float().mean().item())
 
         # Create a bar plot to visualize the average black pixels per class
-        fig = go.Figure(data=[go.Bar(
+        trace1 = go.Figure(data=[go.Bar(
             x=self.class_labels,
-            y=class_pixel_averages,
+            y=class_pixel_averages_train,
             marker_color='#ff6900',
-            text=[f'{pixel_count:.2f}' for pixel_count in class_pixel_averages],  # Only the numerical values
-            textposition='auto'  # Position the text on top of the bars
-
+            text=[f'{pixel_count:.2f}' for pixel_count in class_pixel_averages_train],  # Only the numerical values
+            textposition='auto'
         )])
 
+        trace2 = go.Figure(data=[go.Bar(
+            x=self.class_labels,
+            y=class_pixel_averages_test,
+            marker_color='#ff6900',
+            text=[f'{pixel_count:.2f}' for pixel_count in class_pixel_averages_test],  # Only the numerical values
+            textposition='auto'
+        )])
+
+        trace1.update_traces(texttemplate='%{text:.2s}', textposition='inside')
+        trace2.update_traces(texttemplate='%{text:.2s}', textposition='inside')
+
+        # make a subplots
+        fig = make_subplots(rows=2, cols=1, subplot_titles=('Train Set', 'Test Set'))
+
+        fig.add_trace(trace1.data[0], row=1, col=1)
+        fig.add_trace(trace2.data[0], row=2, col=1)
+
+        # update layout
         fig.update_layout(
-            yaxis=dict(showticklabels=False),
-            title='Average Black Pixels per Class in Fashion MNIST',
-            titlefont=dict(size=16, color='#000000'),
+            title='Average Number of Black Pixels per Class',
             xaxis=dict(tickangle=45),
             showlegend=False,
-            margin=dict(t=80, b=80, l=60, r=80)
+            margin=dict(t=80, b=80, l=40, r=20)
         )
+
+        fig.update_xaxes(tickangle=45, row=2, col=1)
+        fig.update_layout(height=600)
+        fig.update_layout(titlefont=dict(size=16, color='#000000'))
+        fig.update_yaxes(tickfont_color='#000000', row=1, col=1)
+        fig.update_yaxes(tickfont_color='#000000', row=2, col=1)
+        fig.update_xaxes(tickfont_color='#000000', row=1, col=1)
+        fig.update_xaxes(tickfont_color='#000000', row=2, col=1)
 
         fig.show()
 
